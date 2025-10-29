1753c801c01f702b6d3ea81bee827279e0fff4f1
vlecabr <vitartasleca@gmail.com>
2025-10-29 11:01:56 -0300

    vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // muda de 'dist' para 'build'
+    outDir: 'build', // Vercel espera a pasta 'build'
+    assetsDir: 'assets', // pasta para JS/CSS/imagens
+    sourcemap: false,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 });
