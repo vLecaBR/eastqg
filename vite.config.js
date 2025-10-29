792a9b1162aad8902a25716e0c7b06cc95df053b
vlecabr <vitartasleca@gmail.com>
2025-10-29 11:15:49 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Vercel espera a pasta 'build'
-    assetsDir: 'assets', // pasta para JS/CSS/imagens
-    sourcemap: false,
+    outDir: "dist",
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+  base: "/", // raiz do site
 });
