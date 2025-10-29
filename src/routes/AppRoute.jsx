9b541f55daee6d839b69f19634bb0dcbbcd23e52
vlecabr <vitartasleca@gmail.com>
2025-10-29 08:56:15 -0300

    removendo quote e saved

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -4,8 +4,6 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Home/HomePage.jsx";
 import ServicesPage from "../pages/ServicesPage/ServicesPage.jsx";
 import ServiceDetailPage from "../pages/ServicesPage/ServiceDetail/ServiceDetailPage.jsx";
-import SavedServicesPage from "../pages/SavedServices/SavedServicesPage.jsx";
-import QuotePage from "../pages/Quote/QuotePage.jsx";
 import AboutPage from "../pages/About/AboutPage.jsx";
 import ContactPage from "../pages/Contact/ContactPage.jsx";
 import ProductsPage from "../pages/Products/Products.jsx";
@@ -13,13 +11,8 @@ import ProductDetails from "../pages/Products/ProductDetails/ProductDetails.jsx"
 
 export default function AppRoutes({
   services,
-  savedServices,
-  quote,
   favoriteProducts,
   cart,
-  onToggleSaved,
-  onAddToQuote,
-  onRemoveFromQuote,
   onToggleFavoriteProduct,
   onAddToCart,
 }) {
@@ -27,41 +20,17 @@ export default function AppRoutes({
     <Routes>
       <Route
         path="/"
-        element={
-          <HomePage
-            services={services}
-            onToggleSaved={onToggleSaved}
-            onAddToQuote={onAddToQuote}
-            savedServices={savedServices}
-            quote={quote}
-          />
-        }
+        element={<HomePage services={services} />}
       />
 
       <Route
         path="/services"
-        element={
-          <ServicesPage
-            services={services}
-            onToggleSaved={onToggleSaved}
-            onAddToQuote={onAddToQuote}
-            savedServices={savedServices}
-            quote={quote}
-          />
-        }
+        element={<ServicesPage services={services} />}
       />
 
       <Route
         path="/service/:id"
-        element={
-          <ServiceDetailPage
-            services={services}
-            onToggleSaved={onToggleSaved}
-            onAddToQuote={onAddToQuote}
-            savedServices={savedServices}
-            quote={quote}
-          />
-        }
+        element={<ServiceDetailPage services={services} />}
       />
 
       <Route
@@ -77,30 +46,6 @@ export default function AppRoutes({
       />
 
       <Route path="/product/:id" element={<ProductDetails />} />
-
-      <Route
-        path="/saved"
-        element={
-          <SavedServicesPage
-            services={services}
-            savedServices={savedServices}
-            quote={quote}
-            onToggleSaved={onToggleSaved}
-            onAddToQuote={onAddToQuote}
-          />
-        }
-      />
-
-      <Route
-        path="/quote"
-        element={
-          <QuotePage
-            services={services}
-            quote={quote}
-            onRemoveFromQuote={onRemoveFromQuote}
-          />
-        }
-      />
 
       <Route path="/about" element={<AboutPage />} />
       <Route path="/contact" element={<ContactPage />} />
